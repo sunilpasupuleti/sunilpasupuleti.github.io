c48f18a513aebc5e67ea33585a313e033fbedbd7
Sunil Kumar Pasupuleti <sunilpasupuleti@outlook.com>
2022-09-15 14:18:13 +0530

    made changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,63 @@ AOS.init({
   },
 });
 
-// document.documentElement.style.setProperty("--hue-color", "150");
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while (i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++;
+  }
+  return;
+}
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+
+  let i = 0;
+  while (letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+  await waitForMs(500);
+}
+
+function waitForMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const carouselText = [
+  { text: "Sunil", color: "var(--first-color)" },
+  { text: "Kumar", color: "hsl(340, 69%, 61%)" },
+];
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css("color", color);
+}
+
+async function carousel(carouselList, eleRef) {
+  var i = 0;
+  while (i < carouselList.length) {
+    updateFontColor(eleRef, carouselList[i].color);
+    await typeSentence(carouselList[i].text, eleRef);
+    await waitForMs(500);
+    await deleteSentence(eleRef);
+    await waitForMs(300);
+    i++;
+  }
+}
+
+async function startTyping() {
+  await typeSentence("Hi, I am ", "#type__title");
+  await carousel(carouselText, "#featured__title");
+  await deleteSentence("#type__title");
+  await waitForMs(500);
+  startTyping();
+}
+
+startTyping();
 
 const cursor = document.querySelector(".moving__cursor");
 var timeout;
@@ -68,6 +124,13 @@ document.onreadystatechange = () => {
     $(".footer").fadeIn();
     $(".header").fadeIn();
     $(".main").fadeIn();
+
+    if (window.location.hash) {
+      const ele = document.querySelector(window.location.hash);
+      setTimeout(() => {
+        ele.scrollIntoView();
+      }, 100);
+    }
   }
 };
 
@@ -339,134 +402,6 @@ themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
   themeButton.classList.toggle("uil-sun");
 });
-
-// List of sentences
-var _CONTENT = ["Hi, I'am Sunil Kumar"];
-
-// Current sentence being processed
-var _PART = 0;
-
-// Character number of the current sentence being processed
-var _PART_INDEX = 0;
-
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL;
-
-// Element that holds the text
-var _ELEMENT = document.querySelector("#type__title");
-
-// Cursor element
-var _CURSOR = document.querySelector("#cursor");
-
-// Implements typing effect
-function Type() {
-  // Get substring with 1 characater added
-  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-  _ELEMENT.innerHTML = text;
-  _PART_INDEX++;
-
-  // If full sentence has been displayed then start to delete the sentence after some time
-  if (text === _CONTENT[_PART]) {
-    // Hide the cursor
-    _CURSOR.style.display = "none";
-
-    clearInterval(_INTERVAL_VAL);
-    setTimeout(function () {
-      _INTERVAL_VAL = setInterval(Delete, 50);
-    }, 1000);
-  }
-}
-
-// Implements deleting effect
-function Delete() {
-  // Get substring with 1 characater deleted
-  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-  _ELEMENT.innerHTML = text;
-  _PART_INDEX--;
-
-  // If sentence has been deleted then start to display the next sentence
-  if (text === "") {
-    clearInterval(_INTERVAL_VAL);
-
-    // If current sentence was last then display the first one, else move to the next
-    if (_PART == _CONTENT.length - 1) _PART = 0;
-    else _PART++;
-
-    _PART_INDEX = 0;
-
-    // Start to display the next sentence after some time
-    setTimeout(function () {
-      _CURSOR.style.display = "inline-block";
-      _INTERVAL_VAL = setInterval(Type, 100);
-    }, 200);
-  }
-}
-
-// Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 100);
-
-// List of sentences
-var _CONTENT1 = [
-  "Full Stack Developer",
-  "Freelancer",
-  "Frontend Developer",
-  "Backend Developer",
-];
-
-// Current sentence being processed
-var _PART1 = 0;
-
-// Character number of the current sentence being processed
-var _PART_INDEX1 = 0;
-
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL1;
-
-// Element that holds the text
-var _ELEMENT1 = document.querySelector("#type__roles");
-
-// Implements typing effect
-function Type1() {
-  // Get substring with 1 characater added
-  var text = _CONTENT1[_PART1].substring(0, _PART_INDEX1 + 1);
-  _ELEMENT1.innerHTML = text;
-  _PART_INDEX1++;
-
-  // If full sentence has been displayed then start to delete the sentence after some time
-  if (text === _CONTENT1[_PART1]) {
-    clearInterval(_INTERVAL_VAL1);
-    setTimeout(function () {
-      _INTERVAL_VAL1 = setInterval(Delete1, 30);
-    }, 1000);
-  }
-}
-
-// Implements deleting effect
-function Delete1() {
-  // Get substring with 1 characater deleted
-  var text = _CONTENT1[_PART1].substring(0, _PART_INDEX1 - 1);
-  _ELEMENT1.innerHTML = text;
-  _PART_INDEX1--;
-
-  // If sentence has been deleted then start to display the next sentence
-  if (text === "") {
-    clearInterval(_INTERVAL_VAL1);
-
-    // If current sentence was last then display the first one, else move to the next
-    if (_PART1 == _CONTENT1.length - 1) _PART1 = 0;
-    else _PART1++;
-
-    _PART_INDEX1 = 0;
-
-    // Start to display the next sentence after some time
-    setTimeout(function () {
-      _INTERVAL_VAL1 = setInterval(Type1, 50);
-    }, 200);
-  }
-}
-
-// Start the typing effect on load
-_INTERVAL_VAL1 = setInterval(Type1, 50);
 
 // send mail
 
